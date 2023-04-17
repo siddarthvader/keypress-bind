08849c422752598814bf7f7dee9680c41292a147
siddharth <me@siddharthjain.in>
2023-04-18 00:59:52 +0530

    changed loading strategry for script

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -13,7 +13,7 @@ export default function Document() {
           src="keypress/keypress-2.1.5.min.js"
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
       </body>
     </Html>
