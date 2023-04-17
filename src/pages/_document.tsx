baae6b903e357a761f3bf83dba04ad710e3a5809
siddharth <me@siddharthjain.in>
2023-04-18 00:31:18 +0530

    document changes for script add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/keypress/2.1.5/keypress.min.js"
-          integrity="sha512-JjqUcblrwIZTQBPL/azOhVHwq5uaeXkKzq7da3JZEP14Zg926vZZVhRhBBi+L2pWYquZ6r5P8OZYDYgOChtkOw=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        ></script>
-      </Head>
+      <Head></Head>
+
       <body className="w-[100vw] h-[100vh]">
         <Main />
         <NextScript />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/keypress/2.1.5/keypress.min.js"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+          strategy="afterInteractive"
+        />
       </body>
     </Html>
   );
